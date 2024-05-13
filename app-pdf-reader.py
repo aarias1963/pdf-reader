0133b3a40249b37360ebf1ea3680cd4dabbf7a81
aarias1963 <61782547+aarias1963@users.noreply.github.com>
2024-05-13 15:20:47 +0200

    Update app-pdf-reader.py

--- app-pdf-reader.py
+++ app-pdf-reader.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import PyPDF2
+import io
 
 # Crear un título para la aplicación
 st.title("Buscar palabras en un archivo PDF")
 
 # Crear un uploader de archivos
-uploaded_file = st.file_uploader("Seleccione un archivo PDF", type=["pdf"])
+uploaded_file = st.file_uploader("Seleccione un archivo PDF", type=["pdf"], accept=["application/pdf"])
 
 # Crear un campo de texto para ingresar las palabras a buscar
 search_words = st.text_input("Buscar palabras")
@@ -16,7 +17,9 @@ search_button = st.button("Buscar")
 # Procesar la solicitud cuando se carga el archivo PDF
 if uploaded_file is not None and search_button:
     # Leer el archivo PDF
-    pdf_file = uploaded_file.getvalue("rb")
+    pdf_file = io.BytesIO()
+    pdf_file.write(uploaded_file)
+    pdf_file.seek(0)  # Reset the file pointer to the beginning
     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 
     # Leer el contenido del archivo PDF
