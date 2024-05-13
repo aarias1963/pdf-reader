e89f9b4840a96133c50eefeed2e91f790e20e30a
aarias1963 <61782547+aarias1963@users.noreply.github.com>
2024-05-13 15:41:37 +0200

    Update app-pdf-reader.py

--- app-pdf-reader.py
+++ app-pdf-reader.py
@@ -21,14 +21,15 @@ if search_button:
         # Leer el archivo PDF
         pdf_file = uploaded_file.read()
         pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_file))
+
+        # Leer el contenido del archivo PDF
         pdf_text = ""
         for page in pdf_reader.pages:
             pdf_text += page.extract_text()
 
-    
-    # Buscar las palabras en el contenido del archivo PDF
-    results = [word for word in search_words.split() if word in pdf_text]
+        # Buscar las palabras en el contenido del archivo PDF
+        results = [word for word in search_words.split() if word in pdf_text]
 
-    # Mostrar los resultados
-    st.write("Resultados de la búsqueda:")
-    st.write(results)
+        # Mostrar los resultados
+        st.write("Resultados de la búsqueda:")
+        st.write(results)
