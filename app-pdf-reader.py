3f139b9916a0f49509efe3c5f0d697d9b9fe2587
aarias1963 <61782547+aarias1963@users.noreply.github.com>
2024-05-13 15:14:10 +0200

    Add files via upload

---
+++ app-pdf-reader.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import PyPDF2
+
+# Crear un título para la aplicación
+st.title("Buscar palabras en un archivo PDF")
+
+# Crear un uploader de archivos
+uploaded_file = 'prueba.pdf'
+
+# Crear un campo de texto para ingresar las palabras a buscar
+search_words = st.text_input("Buscar palabras")
+
+# Crear un botón para buscar palabras
+search_button = st.button("Buscar")
+
+# Procesar la solicitud cuando se carga el archivo PDF
+if uploaded_file is not None and search_button:
+    # Leer el archivo PDF
+    pdf_file = uploaded_file.getvalue("rb")
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
+    # Leer el contenido del archivo PDF
+    pdf_text = ""
+    for page in pdf_reader.pages:
+        pdf_text += page.extractText()
+
+    # Buscar las palabras en el contenido del archivo PDF
+    results = [word for word in search_words.split() if word in pdf_text]
+
+    # Mostrar los resultados
+    st.write("Resultados de la búsqueda:")
+    st.write(results)
